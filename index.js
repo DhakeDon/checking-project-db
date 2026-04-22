655379d27fb964a3f9c241281478c1dc37602760
DhakeDon <Akshitdhake@gmail.com>
2026-04-22 12:38:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.get('/roll', async (req, res) => {
 
 // Home
 app.get('/', (req, res) => {
-  res.send('🚀 DB display app running with webook ');
+  res.send('🚀 DB display app running with webook and solve ');
 });
 
 app.listen(PORT, () => {
