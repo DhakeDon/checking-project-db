2e1c78ac27658c5ead899fe7b4c5b0d17cc82759
DhakeDon <Akshitdhake@gmail.com>
2026-04-22 13:47:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.get('/roll', async (req, res) => {
 
 // Home
 app.get('/', (req, res) => {
-  res.send('🚀 DB display app running with webhook  hohoho ');
+  res.send('🚀 DB display app running with webhook  hahahah ');
 });
 
 app.listen(PORT, () => {
